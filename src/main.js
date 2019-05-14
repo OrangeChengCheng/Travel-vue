6eb0ae42b4b2a59ec03a204ae00b27c38db9c6dd
chengkai <kai.cheng01@weimob.com>
2019-05-14 11:01:59 +0800

    增加头部

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 // 手机端样式
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 // 手机端1像素边框
-import './assets/styles/border.css'
+import 'styles/border.css'
 // 手机端点击延迟
 import fastClick from 'fastclick'
+// iconfont引入
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
@@ -20,4 +22,3 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
-
